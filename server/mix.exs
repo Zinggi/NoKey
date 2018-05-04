b9cc2296944a50d5fb1cfd9e735f7a47fd39d105
Flo <nope@nomail.com>
2018-05-04 13:52:33 +0200

    bump server version

--- server/mix.exs
+++ server/mix.exs
@@ -5,7 +5,7 @@ defmodule NoPass.Mixfile do
     [
       app: :no_pass,
       # TODO: increment for release
-      version: "0.1.3",
+      version: "0.2.0",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
